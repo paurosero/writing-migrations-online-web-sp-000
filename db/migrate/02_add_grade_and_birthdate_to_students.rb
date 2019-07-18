bde60e946a662cda90172fcebe81deb742da009f
Paul Rosero <paul_rosero@yahoo.com>
2019-07-18 17:00:12 +0000

    Automatically backed up by Learn

--- db/migrate/02_add_grade_and_birthdate_to_students.rb
+++ db/migrate/02_add_grade_and_birthdate_to_students.rb
@@ -1,7 +1,8 @@
-AddGradeAndBirthdateToStudents < ActiveRecord::Migration
-def change
+class AddGradeAndBirthdateToStudents < ActiveRecord::Migration
+  def change
     add_column :students do |t|
       t.integer :grade
       t.string :birthdate
     end
   end
+end  
